a54d7158229e01d58705e89c755457c947b5215d
Luis Orozco <111475612+Luisorozco43@users.noreply.github.com>
2024-06-15 05:59:13 +0000

    Correccion del juego, faltaba el randomizer del player 2 y otras funciones

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
+import random
 
 def main():
 
-    player1 = ""
-    player2 = ""
 
-    while player1 not in ["rock", "paper", "scissors"]:
-        player1 = input("Player 1, enter rock, paper, or scissors: ")
-
-    while player2 not in ["rock", "paper", "scissors"]:
-        player2 = input("Player 2, enter rock, paper, or scissors: ")
+    counter = 0
+    player1wins = 0
+    player2wins = 0
+    while counter < 5:
         
-    result = rock_paper_scissors(player1, player2)
-    print(result)
+        player1 = ""
+        player2 = ""
 
+        while player1 not in ["rock", "paper", "scissors"]:
+            player1 = input("Player 1, enter rock, paper, or scissors: ")
+
+        player2 = random.choice(["rock", "paper", "scissors"])
+        print(f"Player 2: {player2}")
+
+        result = rock_paper_scissors(player1, player2)
+        if result == "Player 1 wins!":
+            player1wins += 1
+        else:
+            player2wins += 1
+        
+        counter += 1
+
+    if player1wins > player2wins:
+        print("Player 1 wins the game!")
+    elif player1wins == player2wins:
+        print("It's a tie!")
+    else:
+        print("Player 1 loose the game!")
 
 def rock_paper_scissors(player1, player2):
     if player1 == player2:
